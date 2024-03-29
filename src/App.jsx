55351f2b849f8f8bded6e1755963f03246078f33
Joowon Jang <wndnjs5248@hanmail.net>
2024-03-29 18:31:08 +0900

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet-async";
+import "./App.scss";
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <title>Joowon's PortFolio</title>
+      </Helmet>
+      just started
+    </div>
+  );
+}
+
+export default App;
