ee74c5326baa5f69ee2372c2e1ec452eeb413dbe
Joowon Jang <wndnjs5248@hanmail.net>
2024-04-04 18:02:17 +0900

    refactor: Add project image container

--- src/components/ProjectItem/ProjectPortfolio.jsx
+++ src/components/ProjectItem/ProjectPortfolio.jsx
@@ -6,19 +6,21 @@ import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
 
+import IconGithub from "../../assets/img/icon-velog.png";
+
 import "./projectitem.scss";
 
 const ProjectPortfolio = () => {
   return (
     <div className="project-item">
-      <h2 className="project-item-title">asdf</h2>
-      <p className="project-item-duration">asdfasdf</p>
-      <div className="project-item-detail">
-        <div className="project-item-detail-left">left</div>
-        <div className="project-item-detail-right">
+      <h2 className="project-title">asdf</h2>
+      <p className="project-duration">asdfasdf</p>
+      <div className="project-detail">
+        <div className="project-detail-left">left</div>
+        <div className="project-detail-right">
           <Swiper
             modules={[Autoplay, Pagination]}
-            spaceBetween={50}
+            spaceBetween={30}
             slidesPerView={1}
             autoplay={{
               delay: 5000,
@@ -28,22 +30,31 @@ const ProjectPortfolio = () => {
             pagination={{ type: "fraction", clickable: true }}
           >
             <SwiperSlide>
-              <img
-                alt="project-portfolio-image1"
-                className="project-item-image"
-              />
+              <div className="project-image-container">
+                <img
+                  src={IconGithub}
+                  alt="project-portfolio-image1"
+                  className="project-image"
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-              <img
-                alt="project-portfolio-image2"
-                className="project-item-image"
-              />
+              <div className="project-image-container">
+                <img
+                  src={IconGithub}
+                  alt="project-portfolio-image2"
+                  className="project-image"
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-              <img
-                alt="project-portfolio-image3"
-                className="project-item-image"
-              />
+              <div className="project-image-container">
+                <img
+                  src={IconGithub}
+                  alt="project-portfolio-image3"
+                  className="project-image"
+                />
+              </div>
             </SwiperSlide>
           </Swiper>
         </div>
